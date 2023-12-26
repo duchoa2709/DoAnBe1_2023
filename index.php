860228f09d3d8d942a51b7d36dc59c2c78410350
duchoa2709 <21211tt4819@mail.tdc.edu.vn>
2023-12-26 10:08:05 +0700

    update

--- index.php
+++ index.php
@@ -318,7 +318,7 @@
         <div class="grid grid-rows-3">
             <div class="col-md-4 col-xs-6">
                 <div class="section-title">
-                    <h4 class="title">Top sellingb</h4>
+                    <h4 class="title">Top selling</h4>
                     <div class="section-nav">
                         <div id="slick-nav-3" class="products-slick-nav"></div>
                     </div>
@@ -352,7 +352,7 @@
 
             <div class="col-md-4 col-xs-6">
                 <div class="section-title">
-                    <h4 class="title">Top sellingc</h4>
+                    <h4 class="title">Top selling</h4>
                     <div class="section-nav">
                         <div id="slick-nav-4" class="products-slick-nav"></div>
                     </div>
@@ -387,7 +387,7 @@
 
             <div class="col-md-4 col-xs-6">
                 <div class="section-title">
-                    <h4 class="title">Top sellingd</h4>
+                    <h4 class="title">Top selling</h4>
                     <div class="section-nav">
                         <div id="slick-nav-5" class="products-slick-nav"></div>
                     </div>

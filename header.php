4e86f412bea401393d023aeb25b33f3fd9210c98
duchoa2709 <21211tt4819@mail.tdc.edu.vn>
2023-12-26 10:46:19 +0700

    update

--- header.php
+++ header.php
@@ -50,6 +50,10 @@ if(isset($_SESSION['wishlist'])){
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 
+    <!-- animation -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
 </head>
 
 <body>

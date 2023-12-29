a83cd97f6ec7ea62393fb6c1999efc24185c21fc
duchoa2709 <21211tt4819@mail.tdc.edu.vn>
2023-12-29 13:24:20 +0700

    update

--- header.php
+++ header.php
@@ -56,7 +56,8 @@ $getAllprotypes = $protypes->getAllprotypes();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <!-- <script src="https://cdn.tailwindcss.com"></script> -->
-
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <style>
     /* Customize scrollbar for Chrome, Safari and Opera */
     body::-webkit-scrollbar {
